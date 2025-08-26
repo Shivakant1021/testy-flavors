03ca7f4537230e8619debe0fb542bbec6594d0e0
Shivakant1021 <shivakant011999@gmail.com>
2025-08-27 01:40:01 +0530

    Fix GitHub Pages deployment with BrowserRouter basename and vite config

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import Store from "./redux/Store.js";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={Store}>
-      <App />
+      <BrowserRouter basename="/testy-flavors">
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
