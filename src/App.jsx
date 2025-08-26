427a9f636f2f977ddd6b949611c3fc45f10d407c
Shivakant1021 <shivakant011999@gmail.com>
2025-08-27 01:48:33 +0530

    Fix GitHub Pages deployment with BrowserRouter basename and vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
@@ -8,16 +7,14 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/success"
-          element={<ProtectedRoute element={<Success />} />}
-        />
-        <Route path="/*" element={<Error />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/success"
+        element={<ProtectedRoute element={<Success />} />}
+      />
+      <Route path="/*" element={<Error />} />
+    </Routes>
   );
 };
 
